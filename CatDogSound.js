418fdcf70e04510c31d5fc10bad6de1aa354c7c4
riverinflow <132797989+riverinflow@users.noreply.github.com>
2025-02-21 02:05:39 -0500

    Create CatDogSound.js

---
+++ CatDogSound.js
@@ -0,0 +1,26 @@
+var petDog = "Rex"
+var petCat = "Pepper"
+console.log(petDog);
+console.log(petCat);
+console.log("My pet dog's name is: " + petDog);
+console.log("My pet cat's name is: " + petCat);
+
+var catSound = "purr"
+var dogSound = "woof"
+
+console.log(petDog + " says " + dogSound);
+console.log(petCat + " says " + catSound);
+
+var catSound = "meow"
+
+console.log(petCat + " says " + catSound);
+
+/*
+Rex
+Pepper
+My pet dog's name is: Rex
+My pet cat's name is: Pepper
+Rex says woof
+Pepper says purr
+Pepper says meow
+*/
