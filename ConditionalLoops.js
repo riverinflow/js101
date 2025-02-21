822f3f8f2e9ca1bc6bdbd0f54add4435b1db94e6
riverinflow <132797989+riverinflow@users.noreply.github.com>
2025-02-21 01:45:13 -0500

    Create ConditionalLoops.js

---
+++ ConditionalLoops.js
@@ -0,0 +1,11 @@
+for(var i = 1; i <= 10; i++) {
+    if(i == 1){
+        console.log("Gold Medal")
+}
+else if (i == 2){
+    console.log("Silver Medal")
+}
+else if (i == 3){
+    console.log("Bronze Medal")
+}    
+}
